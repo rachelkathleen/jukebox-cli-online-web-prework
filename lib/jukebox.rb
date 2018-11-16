c54937a9fcb5bbffd696754bdb8b4cc2835e5500
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-16 03:24:06 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,14 @@
+#def say_hello(name)
+#  "Hi #{name}!"
+#end
+
+#puts "Enter your name:"
+#users_name = gets.chomp
+
+#puts say_hello(users_name)
+
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +21,47 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+ puts "I accept the following commands:"
+ puts "- help : displays this help message"
+ puts "- list : displays a list of songs you can play"
+ puts "- play : lets you choose a song to play"
+ puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, i|
+    puts "#{i+1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_to_play = gets.chomp
+  if (1..9).to_a.include?(song_to_play.to_i)
+    puts "Playing #{songs[song_to_play.to_i - 1]}"
+  elsif songs.include?(song_to_play)
+    puts "Playing #{song_to_play}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+ puts "Goodbye"
+end
+
+def run(command)
+  help
+  puts "Please enter a command:"
+  first_selection = gets.chomp
+  if first_selection == "help"
+    help
+  elsif first_selection == "list"
+    list(songs)
+  elsif first_selection == "play"
+    play(songs)
+  else first_selection == "exit"
+    exit_jukebox
+  end
+end
