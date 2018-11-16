ffbc8005bdf60a748d099fb213257ec0cf7bd510
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-16 01:44:40 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -6,3 +6,11 @@
 #users_name = gets.chomp
 
 #puts say_hello(users_name)
+
+def help
+ puts "I accept the following commands:"
+ puts "- help : displays this help message"
+ puts "- list : displays a list of songs you can play"
+ puts "- play : lets you choose a song to play"
+ puts "- exit : exits this program"
+end
